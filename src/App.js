fbfa1f44e88402b254319d5003ca54c051d655ef
Mihail155 <66168800+Mihail155@users.noreply.github.com>
2020-10-04 13:24:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import '@vkontakte/vkui/dist/vkui.css';
 import Home from './panels/Home';
 import Persik from './panels/Persik';
 
+let = bridge.send("VKWebAppAllowNotifications");
+
+
 const App = () => {
 	const [activePanel, setActivePanel] = useState('home');
 	const [fetchedUser, setUser] = useState(null);
